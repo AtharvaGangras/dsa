8e20f5dc2f4e6f2d64c68c8c28be797d357f6dc9
Atharva Gangras <acgangras@gmail.com>
2023-01-21 07:39:24 +0530

    add optimal approach to sortcolors

--- array/arraystriversheetday1to4/sortcolors.cpp
+++ array/arraystriversheetday1to4/sortcolors.cpp
@@ -44,23 +44,32 @@ public:
         // }
 
         //method 3: counting the no. of 0's,1's,2's and then replacing till that.
-        int zeros = 0,ones = 0,twos = 0;
-        for(int i = 0;i<nums.size();i++)
+        // int zeros = 0,ones = 0,twos = 0;
+        // for(int i = 0;i<nums.size();i++)
+        // {
+        //     if(nums[i] == 0) zeros++;
+        //     if(nums[i] == 1) ones++;
+        //     if(nums[i] == 2) twos++;
+        // }
+        // ones += zeros;
+        // twos += ones;
+        // for(int i = 0;i<nums.size();i++)
+        // {
+        //     if(i<zeros) nums[i] = 0;
+        //     else if(i<ones) nums[i] = 1;
+        //     else if(i<twos) nums[i] = 2;
+        // }
+
+        //method 4: 3 pointer approach(optimal approach)
+        int low = 0,mid = 0;
+        int high = nums.size()-1;
+        while(mid<=high)
         {
-            if(nums[i] == 0) zeros++;
-            if(nums[i] == 1) ones++;
-            if(nums[i] == 2) twos++;
-        }
-        ones += zeros;
-        twos += ones;
-        for(int i = 0;i<nums.size();i++)
-        {
-            if(i<zeros) nums[i] = 0;
-            else if(i<ones) nums[i] = 1;
-            else if(i<twos) nums[i] = 2;
+            if(nums[mid] == 0) swap(nums[low++],nums[mid++]);
+            else if(nums[mid] == 1) mid++;
+            else if(nums[mid] == 2) swap(nums[mid],nums[high--]);
         }
 
-        //method 4: 3 pointer approach
 
 
         
