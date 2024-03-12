49ec1760cecdc2444ce1926f5295b6c5ed262869
Atharva Gangras <acgangras@gmail.com>
2024-03-12 12:46:03 +0530

    recursion l1 complete

---
+++ recursion/recursionarraykunalkush.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//given an array check if it is sorted or not.
+bool ifsort(vector<int> v)
+{
+    
+}
+int main()
+{
+    vector<int> v = {1,5,6,65,565};
+    cout<<ifsort(v);
+    cout<<endl;
+}
--- recursion/recursionl1kunalkush.cpp
+++ recursion/recursionl1kunalkush.cpp
@@ -44,6 +44,9 @@ int pod(int n)
 }
 //reverse a number: last digit +_ rev(rest of number)
 // we will use log to get number of digits
+//ways to get number of digits
+// 1. take sum = 0 outside globally and keep updating it. sum = sum*10 + current digit
+//
 int rev(int n)
 { 
         
@@ -54,6 +57,55 @@ int rev(int n)
 
 
 }
+// palindrome function
+bool palin(int n)
+{
+    return n == rev(n);
+}
+bool helper(int n,int len){
+    if(len == 1 || len == 0) return true;
+    int t = n/pow(10,len-1);
+    if(n%10 == t)
+    {
+        n = n% (int)pow(10,len-1);
+        n = n%10;
+        return helper(n,len-2);
+    }
+    return false;
+}
+bool palin2(int n)
+{
+    int len = (int)log10(n)+1; //finds the length of number
+    if(len == 1) return true;
+    int t = n/pow(10,len-1);
+    // cout<<t<<endl;
+    //compare first and last digit
+    if(n%10 == t)
+    {
+        n = n% (int)pow(10,len-1);
+        n = n/10;
+        return helper(n,len-2);
+    }
+    return false;
+}
+
+// count no. of zeroes in a number
+int zeros(int n)
+{
+    if(n == 0) return 0;
+    if(n%10 == 0) return zeros(n/10) +1;
+    return zeros(n/10);
+}
+
+// reduce a no. to zero question if even /2  if odd -1 from it count the no. of steps required
+int reduction(int n)
+{
+    if(n == 0) return 0;
+    if(n&1 == 1) n=n-1; // for odd number
+    else n=n/2; //for even number
+    return 1+ reduction(n);
+
+}
 
 int main()
 {
@@ -62,6 +114,9 @@ int main()
     // cout<<fact(5);
     // cout<<sod(1324555);
     // cout<<pod(1234);
-    cout<<rev(5512321);
+    // cout<<rev(5512321);
+    // cout<<palin2(12321);
+    // cout<<zeros(30210004);
+    cout<<reduction(41);
     cout<<endl;
 }
